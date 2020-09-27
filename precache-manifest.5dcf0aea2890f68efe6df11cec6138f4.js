a3da6a4b37afdd825d966017ba59f172a6fc7f73
Yevhenii <okhremchukjevhenii@.gmail.com>
2020-09-27 20:49:13 +0300

    Updates

---
+++ precache-manifest.5dcf0aea2890f68efe6df11cec6138f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be467474a4e8a7f75aeb11284fa74202",
+    "url": "/React-StyledComponents/index.html"
+  },
+  {
+    "revision": "19b8cfbdfd961edc6966",
+    "url": "/React-StyledComponents/static/css/main.3febc991.chunk.css"
+  },
+  {
+    "revision": "51c68665b6b39644ed01",
+    "url": "/React-StyledComponents/static/js/2.eececfee.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-StyledComponents/static/js/2.eececfee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19b8cfbdfd961edc6966",
+    "url": "/React-StyledComponents/static/js/main.2bbad2dd.chunk.js"
+  },
+  {
+    "revision": "f54265cbb87e228946df",
+    "url": "/React-StyledComponents/static/js/runtime-main.0f39dc41.js"
+  }
+]);
